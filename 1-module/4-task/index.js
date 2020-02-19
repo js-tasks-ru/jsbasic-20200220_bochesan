40630dbf39950d9ad6032ab16dc20ec3dfd3e379
Bochesan <bochesan89@yandex.ru>
2020-02-20 01:37:39 +0300

    Задание 1.4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,16 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
+  str = str.toLowerCase();
+  let checkString = ["1xBet", "XXX"];
 
+  for (let i = 0; i < checkString.length; i++) {
+    const element = checkString[i].toLowerCase();
+    console.log(element);
+    
+    if (str.includes(element)) {
+      return true;
+    }
+  }
+  return false;
 }
