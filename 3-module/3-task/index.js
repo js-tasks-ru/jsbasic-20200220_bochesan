144f098bbba154b42ceab3b31d3077865a5dec05
Bochesan <bochesan89@yandex.ru>
2020-02-26 01:30:13 +0300

    занятие 3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,14 @@
  * @returns {string}
  */
 function camelize(str) {
+  let arr = str.split("-");
+  for (let i = 1; i < arr.length; i++) {
+    let element = arr[i];
+    element = element.split("");
+    element[0] = element[0].toUpperCase();
+    element = element.join("");
+    arr[i] = element;
+  }
+  arr = arr.join("");
+  return arr;
 }
