726d0f04277e60f32f81e8735b67a93ee977ce44
Bochesan <bochesan89@yandex.ru>
2020-02-26 01:13:07 +0300

    занятие 3.2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,15 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let arr = str.split(" ");
+  arr = arr.join(",");
+  arr = arr.split(",");
+
+  arr.sort((a, b) => a - b);
+
+  let result = {};
+  result.min = +arr[0];
+  result.max = +arr[arr.length - 1];
+
+  return result;
 }
