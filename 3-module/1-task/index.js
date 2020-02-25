bf33ef62f692292c78d0081533d9d3af8feb3421
Bochesan <bochesan89@yandex.ru>
2020-02-26 00:10:35 +0300

    занятие 3.1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,15 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let humansArr = [];
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+
+    if (element.age <= age) {
+      humansArr.push(element.name + ", " + element.balance);
+    }
+  }
+  let humans = humansArr.join("\n");
+
+  return humans;
 }
