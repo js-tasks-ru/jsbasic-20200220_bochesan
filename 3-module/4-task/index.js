c9090bd583ea34b07555195706fab9f51db1d17f
Bochesan <bochesan89@yandex.ru>
2020-02-26 01:36:53 +0300

    занятие 3.4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -3,4 +3,13 @@
  * @returns {string[]}  объект
  */
 function namify(users) {
+  let names = [];
+
+  for (let i = 0; i < users.length; i++) {
+    const element = users[i];
+
+    names.push(element.name);
+  }
+
+  return names;
 }
