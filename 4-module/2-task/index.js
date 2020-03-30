db95c655c2d6d38747c57e42772eb27b49c1aaaa
Pavel Zabolotny <p.zabolotny@bi.zone>
2020-03-30 20:12:49 +0300

    4 module 2 task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,13 @@
+/* eslint-disable indent */
 /**
  * @param {HTMLTableElement} table
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    for (let i = 0; i < table.tBodies[0].rows.length; i++) {
+        const element = table.tBodies[0].rows[i];
+        for (let j = 0; j < element.cells.length; j++) {
+            element.cells[i].style.backgroundColor = "red";
+        }
+    }
 }
