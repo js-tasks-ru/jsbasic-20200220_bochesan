c6613f57f277cd508b30b474e9e1e5d803b7935f
Bochesan <bochesan89@yandex.ru>
2020-09-07 00:22:00 +0300

    sort table

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -25,5 +25,67 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+
+  this.createTable = () => {
+    let tbody = "";
+
+    for (let i = 0; i < items.length; i++) {
+      const row = items[i];
+      let tr = "";
+
+      for (const key in row) {
+        if (row.hasOwnProperty(key)) {
+          tr += `<td>${row[key]}</td>`;
+        }
+      }
+      tbody += `<tr>${tr}</tr>`;
+    }
+
+    let table = `<thead>
+                  <td>Name</td>
+                  <td>Age</td>
+                  <td>Salary</td>
+                  <td>City</td>
+                </thead>
+                <tbody>
+                  ${tbody}
+                </tbody>`;
+
+    this.el.innerHTML = table;
+  }
+
+  this.createTable();
+
+  this.sort = (column, desc = false) => {
+    let index;
+
+    switch (column) {
+      case 0:
+        index = 'name';
+        break;
+      case 1:
+        index = 'age';
+      break;
+      case 2:
+        index = 'salary';
+      break;
+      case 3:
+        index = 'city';
+      break;
+      default:
+        return;
+    }
+    
+    if ((column == 1 || column == 2) && desc) {
+      items.sort((a, b) => b[index] - a[index]);
+    } else if ((column == 1 || column == 2) && !desc) {
+      items.sort((a, b) => a[index] - b[index]);
+    } else if ((column == 0 || column == 4) && !desc) {
+      items.sort((a, b) => a[index].toLowerCase() > b[index].toLowerCase() ? 1 : -1);
+    } else if ((column == 0 || column == 4) && desc) {
+      items.sort((a, b) => a[index].toLowerCase() < b[index].toLowerCase() ? 1 : -1);
+    }
+
+    this.createTable();
+  };
 }
